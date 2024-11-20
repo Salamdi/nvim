2bf15070eba09ba902768cfd8c76aeb70693a0b6
Salamdi <dinislam91@gmail.com>
2024-11-20 21:55:39 +0100

    fix no macros recording indication

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,21 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup({
-      options = {
-        -- theme = 'onelight',
-        theme = 'dracula',
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				-- theme = 'onelight',
+				theme = "dracula",
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
+		})
+	end,
 }
