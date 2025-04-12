9692e5806b36f9bd49aa99369acdafc4e45033aa
salamdi <dinislam91@gmail.com>
2025-04-12 15:52:20 +0300

    fix lazy-lock.json

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -20,24 +20,7 @@ return {
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
 		image = {
-			enabled = true,
-			img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments" },
-			math = { enabled = true },
-			latex = {
-				font_size = "Large", -- see https://www.sascha-frank.com/latex-font-size.html
-				-- for latex documents, the doc packages are included automatically,
-				-- but you can add more packages here. Useful for markdown documents.
-				packages = { "amsmath", "amssymb", "amsfonts", "amscd", "mathtools" },
-				tpl = [[
-        \documentclass[preview,border=2pt,varwidth,12pt]{standalone}
-        \usepackage{${packages}}
-        \begin{document}
-        ${header}
-        { \${font_size} \selectfont
-          \color[HTML]{${color}}
-        ${content}}
-        \end{document}]],
-			},
+      -- configs
 		},
 		win = { enabled = true },
 	},
