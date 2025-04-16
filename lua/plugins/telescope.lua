53fbdadd6a2e553b6a64483fee99a1340bb609cb
salamdi <dinislam91@gmail.com>
2025-04-16 20:27:26 +0300

    update config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,16 +3,16 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", function()
-        builtin.find_files({ hidden = true })
-      end, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-      vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
-    end,
+    -- config = function()
+    --   local builtin = require("telescope.builtin")
+    --   vim.keymap.set("n", "<leader>ff", function()
+    --     builtin.find_files({ hidden = true })
+    --   end, {})
+    --   vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    --   vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    --   vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    --   vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
+    -- end,
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
